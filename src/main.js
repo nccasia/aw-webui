fa030c3f6fde26ada2fcb87e3f9d758b6ad1fdfe
Duong Nguyen Dai <hello@mrdnd.dev>
2022-04-12 23:15:50 +0700

    add search function and fix lint

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ Vue.use(VueGtm, {
     // Add URL query string when loading gtm.js with GTM ID (required when using custom environments)
     gtm_auth: '8ZBCHAhTuiKaTAWgzBm4Ow',
     gtm_preview: 'env-1',
-    gtm_cookies_win: 'x'
+    gtm_cookies_win: 'x',
   },
   defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
   compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
@@ -86,7 +86,7 @@ Vue.use(VueGtm, {
   loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
   vueRouter: router, // Pass the router instance to automatically sync with router (optional)
   ignoredViews: ['home'], // Don't trigger events for specified router names (optional)
-  trackOnNextTick: false // Whether or not call trackView in Vue.nextTick
+  trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
 });
 
 // Setup Vue app

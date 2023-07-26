ee1dd2cea08d1d699d19e7f452a13bb052ec1b04
Duong Nguyen Dai <hello@mrdnd.dev>
2023-07-26 08:58:14 +0700

    change server endpoint

--- src/util/awclient.js
+++ src/util/awclient.js
@@ -1,11 +1,11 @@
 import { AWClient } from 'aw-client';
 
-let baseURL = 'http://tracker.komu.vn:5600';
+let baseURL = 'https://tracker-api.komu.vn/';
 
 // If running with `npm node dev`, use testing server as origin.
 // Works since CORS is enabled by default when running `aw-server --testing`.
 if (!PRODUCTION) {
-  baseURL = AW_SERVER_URL || 'http://tracker.komu.vn:5600';
+  baseURL = AW_SERVER_URL || 'https://tracker-api.komu.vn/';
 }
 
 const awc = new AWClient('aw-webui', { testing: !PRODUCTION, baseURL });

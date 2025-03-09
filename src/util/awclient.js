1f6dc2965bd7558dfb01292a9e5cba5a846aff42
Duy Nguyen <hoangduy06104@gmail.com>
2025-03-09 20:14:53 +0700

    update

--- src/util/awclient.js
+++ src/util/awclient.js
@@ -1,11 +1,11 @@
 import { AWClient } from 'aw-client';
 
-let baseURL = 'https://tracker-api.nccsoft.vn/';
+let baseURL = 'https://tracker-api.komu.vn/';
 
 // If running with `npm node dev`, use testing server as origin.
 // Works since CORS is enabled by default when running `aw-server --testing`.
 if (!PRODUCTION) {
-  baseURL = process.env.AW_SERVER_URL || 'https://tracker-api.nccsoft.vn/';
+  baseURL = process.env.AW_SERVER_URL || 'https://tracker-api.komu.vn/';
 }
 
 const awc = new AWClient('aw-webui', { testing: !PRODUCTION, baseURL });

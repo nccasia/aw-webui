76aae893ab743e9926e67efb439f5b698875a7ce
DTCode <daithien2004@gmail.com>
2025-12-15 15:07:51 +0700

    update: update baseUrl and .env.sample

--- src/util/awclient.js
+++ src/util/awclient.js
@@ -1,12 +1,6 @@
 import { AWClient } from 'aw-client';
 
-let baseURL = process.env.VUE_APP_DOMAIN;
-
-// If running with `npm node dev`, use testing server as origin.
-// Works since CORS is enabled by default when running `aw-server --testing`.
-if (!PRODUCTION) {
-  baseURL = process.env.VUE_APP_AW_SERVER_URL || 'http://localhost:5600';
-}
+let baseURL = process.env.VUE_APP_AW_SERVER_URL || 'http://localhost:5600';
 
 const awc = new AWClient('aw-webui', { testing: !PRODUCTION, baseURL });
 
